da733cdc57a19dbb4268729aaa03cfb0db325855
hojin35 <66587008+hojin35@users.noreply.github.com>
2022-05-06 00:05:15 +0900

    Add project files.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(505, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 94);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(45, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(501, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 95);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(45, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(410, 164);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(45, 259);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(409, 164);
+            this.listBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+
+namespace WinFormsApp2
+{
+
+    public partial class Form1 : Form
+    {
+        static string targetURL = "https://kr.api.riotgames.com/";
+
+        static string byName = "lol/summoner/v4/summoners/by-name/";
+        static string getGameId = "lol/spectator/v4/active-games/by-summoner/";
+        const string userName = "농고 심스트";
+        const string api_key = "your api_key_input";
+        const string getChampionId = "http://ddragon.leagueoflegends.com/cdn/12.8.1/data/ko_KR/champion.json";
+
+        Dictionary<string,string> championTable = new Dictionary<string,string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        User u = new User();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetChampionId();
+        }
+        private void GetChampionId()
+        {
+            string result = getResults(getChampionId);
+            var list = JObject.Parse(result);
+            foreach(var index in list["data"])
+            {
+                // 챔피언 이름 & 아이디 저장
+                var championList = index.First["key"].ToString();
+                championTable[index.First["key"].ToString()] = index.First["id"].ToString();
+            }
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string url = string.Format(@"{0}{1}{2}?api_key={3}", targetURL, byName, userName, api_key);
+            string result = getResults(url);
+
+            var a = JObject.Parse(result);
+
+            label1.Text = a["id"].ToString();
+            u.Id = a["id"].ToString();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (u.Id != null)
+            {
+                string url = string.Format(@"{0}{1}{2}?api_key={3}", targetURL, getGameId, u.Id, api_key);
+                string result = getResults(url);
+                if (result == "")
+                {
+                    label1.Text = "현재 게임중이 아닙니다.";
+                    return;
+                }
+
+                Root cur = JsonSerializer.Deserialize<Root>(result);
+                for (int i = 0; i < 5; i++)
+                {
+                    listBox1.Items.Add(cur.participants[i].summonerName + "   " + championTable[cur.participants[i].championId.ToString()]);
+                }
+                for (int i = 5; i < 10; i++)
+                {
+                    listBox2.Items.Add(cur.participants[i].summonerName + "   " + championTable[cur.participants[i].championId.ToString()]);
+                }
+            }
+        }
+        public static string getResults(string url)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                WebClient client = new WebClient();
+                using (Stream data = client.OpenRead(url))
+                {
+                    using (StreamReader reader = new StreamReader(data))
+                    {
+                        string s = reader.ReadToEnd();
+                        result = s;
+
+                        reader.Close();
+                        data.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return result;
+        }
+    }
+    class User
+    {
+        public string Id
+        {
+            get; set;
+        }
+        public string UserName
+        {
+            get; set;
+        }
+    }
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Perks
+    {
+        public List<int> perkIds { get; set; }
+        public int perkStyle { get; set; }
+        public int perkSubStyle { get; set; }
+    }
+
+    public class Participant
+    {
+        public int teamId { get; set; }
+        public int spell1Id { get; set; }
+        public int spell2Id { get; set; }
+        public int championId { get; set; }
+        public int profileIconId { get; set; }
+        public string summonerName { get; set; }
+        public bool bot { get; set; }
+        public string summonerId { get; set; }
+        public List<object> gameCustomizationObjects { get; set; }
+        public Perks perks { get; set; }
+    }
+
+    public class Observers
+    {
+        public string encryptionKey { get; set; }
+    }
+
+    public class BannedChampion
+    {
+        public int championId { get; set; }
+        public int teamId { get; set; }
+        public int pickTurn { get; set; }
+    }
+
+    public class Root
+    {
+        public long gameId { get; set; }
+        public int mapId { get; set; }
+        public string gameMode { get; set; }
+        public string gameType { get; set; }
+        public int gameQueueConfigId { get; set; }
+        public List<Participant> participants { get; set; }
+        public Observers observers { get; set; }
+        public string platformId { get; set; }
+        public List<BannedChampion> bannedChampions { get; set; }
+        public long gameStartTime { get; set; }
+        public int gameLength { get; set; }
+    }
+
+
+}
